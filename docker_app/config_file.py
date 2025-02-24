b803613ce5a7964b3a9e34b9f810a75723cf91e3
Nathaniel <nathanielng@users.noreply.github.com>
2025-02-24 22:16:58 +0800

    Update config_file.py

--- docker_app/config_file.py
+++ docker_app/config_file.py
@@ -1,17 +1,17 @@
 class Config:
     # Stack name
     # Change this value if you want to create a new instance of the stack
-    STACK_NAME = "Streamlit"
+    STACK_NAME = "Streamlit-Agents-App"
     
     # Put your own custom value here to prevent ALB to accept requests from
     # other clients that CloudFront. You can choose any random string.
-    CUSTOM_HEADER_VALUE = "My_random_value_58dsv15e4s31"    
+    CUSTOM_HEADER_VALUE = "streamlit_agent_app_a3q4fz"    
     
     # ID of Secrets Manager containing cognito parameters
     # When you delete a secret, you cannot create another one immediately
     # with the same name. Change this value if you destroy your stack and need
     # to recreate it with the same STACK_NAME.
-    SECRETS_MANAGER_ID = f"{STACK_NAME}ParamCognitoSecret12345"
+    SECRETS_MANAGER_ID = f"{STACK_NAME}ParamCognitoSecret20250224"
 
     # AWS region in which you want to deploy the cdk stack
     DEPLOYMENT_REGION = "us-east-1"
